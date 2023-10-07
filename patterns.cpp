c51e37a90c456e0a761dd36f84e7145eb85c807c
Khushi Patel <85817860+risingcoder24@users.noreply.github.com>
2023-10-07 19:30:32 +0530

    Update patterns.cpp

--- patterns.cpp
+++ patterns.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 using namespace std;
 
+//fixed number of rows and columns
+
 int main(){
 
     int n;
@@ -15,5 +17,58 @@ int main(){
         }
         cout<<endl;
     }
+    cout<<endl;
+
+    for(int i = 1; i<=n; i++){
+        for(int j = 1;j<=n;j++){
+            cout<<i<<" ";
+        }
+    cout<<endl;
+    }
+    cout<<endl;
+
+    for(int i = 1; i<n; i++){
+        for(int j = n; j>=1; j--){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=n; j++){
+            cout<<j<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+
+    int k = 1;    
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=n; j++){
+            cout<<k<<"    "<<"   ";
+            k++;
+        }
+        cout<<endl;
+      
+        
+    }
+    cout<<endl;
+
+    for(int i = 1; i<=n; i++){
+        for(int j = i; j<=n; j++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+    cout<<endl;
+
+    for(int i = 1; i<=n; i++){
+        for(int j = 1; j<=i; j++){
+            cout<<"*"<<" ";
+        }
+        cout<<endl;
+    }
+    
 return 0;
 }
